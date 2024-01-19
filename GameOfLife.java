2ed567f9979c7716dc1e89f40519d8e3a9d9552c
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2024-01-20 00:04:22 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -188,7 +188,7 @@ public class GameOfLife {
 		{
 			for (int j = 1; j<arr[i].length-1; j++)
 			{
-				System.out.print( " " + arr[i][j]);
+				System.out.print( "  " + arr[i][j]);
 			}
 			System.out.println();
 		}
